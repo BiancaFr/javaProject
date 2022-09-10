3f259f116a027839bc3886cf8c03c6ffc50626de
Felipe Martinez <60937869+flp-cmd@users.noreply.github.com>
2022-09-10 16:52:05 -0300

    Removendo main e adicionando comentários

--- class3.java
+++ class3.java
@@ -1,26 +1,25 @@
 import java.io.*;
-public class class3
+public class class3 //Creating 'class3'
 {
 
-    public static void main (String[] args) throws IOException
-    {
-        String line;
+    public void fixText() throws IOException  
+    {//Creating 'class3' main method
+        String line; //Temporary string that receives the text lines one by one
         String[] lines = new String[78]; //Array containing every text lines
-        int i = 0;
-    try{
-        File f1 = new File("/home/felipe/Documents/JavaProject/Texto01.txt");
-        BufferedReader in = (new BufferedReader(new FileReader(f1)));
+        int i = 0; //While loop counter
+    try{//Try block to read file
+        File f1 = new File("C:/Unicamp/Java Project/javaProject-master/Texto01.txt"); //Create the type variable File to variable 'f1'
+        BufferedReader in = (new BufferedReader(new FileReader(f1))); //Creates a buffer that reads chunks of characters of the 'f1' and stores them 
         
-    while ((line = in.readLine()) != null) {
-        lines[i] = line.replaceAll("[^a-zA-Z0-9 ÁÀÂÃáàâãÉÈÊẼèéêẽÍÌÎĨíìîĩÓÒÔÕóòôõÚÙÛŨúùũûçÇ]", "").toLowerCase();
-        i++;
-}   in.close();
-    }catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-      }
-      for(int k = 0;k<=lines.length;k++){
-      System.out.println(lines[k]);
-      }
+    while ((line = in.readLine()) != null) { //While loop that verifies if the buffer is reading a line not null and stores the value to the string 'line'
+        lines[i] = line.replaceAll("[^a-zA-Z0-9 ÁÀÂÃáàâãÉÈÊẼèéêẽÍÌÎĨíìîĩÓÒÔÕóòôõÚÙÛŨúùũûçÇ]", "").toLowerCase(); //All special characters are removed and the letters are all replaced to lower case letters, the new characters are storage in the 'lines' array
+        i++; // Incrementing the while loop counter by one
+}   in.close();//Closing the buffer
+        }
+        catch (FileNotFoundException e) //Catch block to show error if and error ocurred in the try block
+        {
+        System.out.println("An error occurred."); //Prints the error message
+        e.printStackTrace();//Shows the specified error
+        } 
     }
 }
